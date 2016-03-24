da7fd7fc526edc7c4cae33e24f50fe2ba54f847b
Andrés Pérez Pérez <Andrs Prez Prez>
2016-03-24 17:09:00 +0100

    Added some work on the md5 hashes calculate

---
+++ cagando.py
@@ -0,0 +1,28 @@
+import md5
+shits=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+indicemierdoso=0;
+cacas=["f","o","r","c","a","c","a","s","i","n","u","r","a","n","u","s","d","o","t","h","e","n","e","x","t","d","o"];
+bigCacas=["f","o","r","c","a","c","a","s","i","n","u","r","a","n","u","s","d","o","t","h","e","n","e","x","t","d","o"];
+for folluto in enumerate(shits):
+    esterco=md5.new(shits[indicemierdoso]).hexdigest();
+    cacas[indicemierdoso]=esterco;
+    esterco=md5.new(shits[indicemierdoso].upper()).hexdigest();
+    bigCacas[indicemierdoso] = esterco;
+    indicemierdoso += 1;
+
+
+
+
+
+
+    orines = (bigCacas[cacas.index(md5.new("h").hexdigest())])
+
+
+
+
+
+
+
+
+
+
