889a1b5e8696bf5d80aca466a9eff46b26f2a0cf
Andrés Pérez Pérez <Andrs Prez Prez>
2016-03-27 10:25:20 +0200

    Deleted unused file

--- test.py
+++
@@ -1,9 +0,0 @@
-caca="""
-THIS IS THE LUCKY SHIT. PLEASE DO NOT DELETE IT OR YOU WILL GET 100 YEARS OF BROKEN COMPILATIONS AND SEGMENTATION FAULTS
-
-
-                                       #+++++++++++++++++++++++++++++++++++++++++++++++++++++#
-                                       #++++++++"""t=a.title();caca2="""++++++++++++++++++++++#
-                                       #++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-"""
